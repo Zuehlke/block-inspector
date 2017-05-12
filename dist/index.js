361661c5f08eb1896463d7c61cd32a76c1a0f130
kabl <blaser_k@gmx.ch>
2017-05-12 13:49:31 +0200

    prepare command lines

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const ContractObserve = require("./ContractObserve");
-const Configuration = require("./Configuration");
-new ContractObserve.ContractObserve(Configuration.Configuration.createDefault()).startScanning();
+const commandLineArgs = require('command-line-args');
+const optionDefinitions = [
+    { name: 'abiPath', type: String, multiple: false },
+    { name: 'address', type: String, multiple: false },
+    { name: 'rpcUrl', type: String, multiple: false },
+    { name: 'block', type: String, multiple: false },
+    { name: 'help', alias: 'h', type: Boolean }
+];
+const options = commandLineArgs(optionDefinitions);
+console.log(options);
+//new ContractObserve.ContractObserve(Configuration.Configuration.createDefault()).startScanning(); 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import * as ContractObserve from './ContractObserve';
 import * as Configuration from './Configuration';
+const commandLineArgs = require('command-line-args');
 
+const optionDefinitions = [
+  { name: 'abiPath', type: String, multiple: false},
+  { name: 'address', type: String, multiple: false},
+  { name: 'rpcUrl', type: String, multiple: false },
+  { name: 'block', type: String, multiple: false},
+  { name: 'help', alias: 'h', type: Boolean }
+]
+
+const options = commandLineArgs(optionDefinitions);
+// TODO: https://www.npmjs.com/package/command-line-args
+// TODO: https://www.npmjs.com/package/command-line-usage
+console.log(options);
 
 new ContractObserve.ContractObserve(Configuration.Configuration.createDefault()).startScanning();
